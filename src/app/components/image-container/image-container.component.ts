9cc23243ab8a3d0018b684cce5c305287bf052b7
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2024-11-17 13:11:41 -0300

    img container

--- src/app/components/image-container/image-container.component.ts
+++ src/app/components/image-container/image-container.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-image-container',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './image-container.component.html',
   styleUrl: './image-container.component.scss'
 })
 export class ImageContainerComponent {
-
+  @Input() src: string | null = null
+  @Input() alt: string = ""
 }
