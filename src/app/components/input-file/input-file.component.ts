4611ed679db88c42cfa35413e91a33de226111a0
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2024-11-17 14:07:02 -0300

    input file button

--- src/app/components/input-file/input-file.component.ts
+++ src/app/components/input-file/input-file.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 
 @Component({
   selector: 'app-input-file',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule],
   templateUrl: './input-file.component.html',
   styleUrl: './input-file.component.scss'
 })
